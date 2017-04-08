c3539ab5c5a5ffa664f5a0908533af3b44c3cd51
Derek Poon <thebevil@gmail.com>
2017-04-08 17:25:58 +0100

    Fixed coloured icons in drawer

--- app/src/main/java/com/hf/navigationview/MainActivity.java
+++ app/src/main/java/com/hf/navigationview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hf.navigationview;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setItemIconTintList(null);
         initViews();
     }
 
@@ -50,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         mDrawerToggle.syncState();
-
 
     }
 }
